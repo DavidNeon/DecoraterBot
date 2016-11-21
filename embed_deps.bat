8cace620bf692ef6ef22f3743abee317fc0f16ec
AraHaan <seandhunt_7@yahoo.com>
2016-11-20 23:10:22 -0600

    ...

--- embed_deps.bat
+++ embed_deps.bat
@@ -1,15 +1,15 @@
 @echo off
-"%SystemDrive%\python352\python.exe" "%~dp0\zipmaker\make_zip.py" -s "%~dp0\zipmaker\deps" -o "%~dp0\zipmaker\DecoraterBot-deps"
+"%SystemDrive%\python352\python.exe" "%~dp0\zipmaker\make_zip.py" -b "%~dp0\resources\python35\win32" -s "%~dp0\zipmaker\deps" -e -o "%~dp0\zipmaker\DecoraterBot-deps"
 REM Now for local 3.5.1
-"H:\Python\Python351\python.exe" "%~dp0\zipmaker\make_zip.py" -s "%~dp0\zipmaker\deps" -o "%~dp0\zipmaker\DecoraterBot-deps"
+"H:\Python\Python351\python.exe" "%~dp0\zipmaker\make_zip.py" -b "%~dp0\resources\python35\win32" -s "%~dp0\zipmaker\deps" -e -o "%~dp0\zipmaker\DecoraterBot-deps"
 REM Now for local 3.5.0
-"H:\Python\python350\python.exe" "%~dp0\zipmaker\make_zip.py" -s "%~dp0\zipmaker\deps" -o "%~dp0\zipmaker\DecoraterBot-deps"
+"H:\Python\python350\python.exe" "%~dp0\zipmaker\make_zip.py" -b "%~dp0\resources\python35\win32" -s "%~dp0\zipmaker\deps" -e -o "%~dp0\zipmaker\DecoraterBot-deps"
 REM Now for local 3.4.5
-"H:\Python\Python345\python.exe" "%~dp0\zipmaker\make_zip.py" -s "%~dp0\zipmaker\deps" -o "%~dp0\zipmaker\DecoraterBot-deps"
+"H:\Python\Python345\python.exe" "%~dp0\zipmaker\make_zip.py" -b "%~dp0\resources\python35\win32" -s "%~dp0\zipmaker\deps" -e -o "%~dp0\zipmaker\DecoraterBot-deps"
 REM Now for local 3.4.4
-"H:\Python\Python344\python.exe" "%~dp0\zipmaker\make_zip.py" -s "%~dp0\zipmaker\deps" -o "%~dp0\zipmaker\DecoraterBot-deps"
+"H:\Python\Python344\python.exe" "%~dp0\zipmaker\make_zip.py" -b "%~dp0\resources\python35\win32" -s "%~dp0\zipmaker\deps" -e -o "%~dp0\zipmaker\DecoraterBot-deps"
 REM Now for local 3.4.3
-"H:\Python\Python343\python.exe" "%~dp0\zipmaker\make_zip.py" -s "%~dp0\zipmaker\deps" -o "%~dp0\zipmaker\DecoraterBot-deps"
+"H:\Python\Python343\python.exe" "%~dp0\zipmaker\make_zip.py" -b "%~dp0\resources\python35\win32" -s "%~dp0\zipmaker\deps" -e -o "%~dp0\zipmaker\DecoraterBot-deps"
 REM Now for local 3.4.2
-"H:\Python\Python342\python.exe" "%~dp0\zipmaker\make_zip.py" -s "%~dp0\zipmaker\deps" -o "%~dp0\zipmaker\DecoraterBot-deps"
+"H:\Python\Python342\python.exe" "%~dp0\zipmaker\make_zip.py" -b "%~dp0\resources\python35\win32" -s "%~dp0\zipmaker\deps" -e -o "%~dp0\zipmaker\DecoraterBot-deps"
 pause
